3905e10013f66e842c4c22f5d400bc3eb1d21904
JongHyuckLee <yarn add --dev --exact prettier>
2024-02-01 01:46:47 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => CatService(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+// 고양이 서비스
+class CatService extends ChangeNotifier {
+  //고양이 사진 담을 변수
+  List<String> catImages = [];
+
+  CatService() {
+    getRandomCatImages();
+  }
+
+  // 랜덤 고양이 사진 API 호출
+  void getRandomCatImages() async {
+    Response result = await Dio().get(
+      "https://api.thecatapi.com/v1/images/search?limit=10&mime_types=jpg",
+    );
+
+    for (var i = 0; i < result.data.length; i++) {
+      var map = result.data[i];
+      catImages.add(map["url"]);
+    }
+    notifyListeners();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CatService>(
+      builder: (context, catService, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("랜덤 고양이"),
+            backgroundColor: Colors.amber,
+            actions: [
+              // 좋아요 페이지로 이동
+              IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => FavoritePage()),
+                  );
+                },
+              )
+            ],
+          ),
+
+          // 고양이 사진 목록
+          body: GridView.count(
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            padding: EdgeInsets.all(8),
+            crossAxisCount: 2,
+            children: List.generate(catService.catImages.length, (index) {
+              String catImage = catService.catImages[index];
+              return Image.network(
+                catImage,
+                fit: BoxFit.cover,
+              );
+            }),
+          ),
+        );
+      },
+    );
+  }
+}
+
+// 좋아요 페이지
+class FavoritePage extends StatelessWidget {
+  const FavoritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CatService>(builder: (context, catService, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("좋아요"),
+          backgroundColor: Colors.amber,
+        ),
+      );
+    });
+  }
+}
